5bdb8dcb306834a99f83ebe6b5fd561ce86afd05
bh <bwhellar@gmail.com>
2019-07-16 10:15:19 -0700

    work with the API syntax to return the proper information

--- src/components/SearchCards.js
+++ src/components/SearchCards.js
@@ -3,22 +3,42 @@ import { Form, FormGroup, FormControl, Button } from 'react-bootstrap';
 
 
 class SearchCards extends Component {
+  constructor(){
+    super();
+    
+    this.state = {
+      name: '',
+      cost: ''
+    }
+  }
+  
+  
+  
+  search (){
+    let { name, cost } = this.state;
+    const url = `https://api.scryfall.com/cards/search?order=cmc&q=${name}+pow%3D${cost}`
+    console.log('state', this.state, 'url', url);
+  }
+  
+  
   render() {
     return (
       <Form inline>
         <FormGroup>
           <h2>Name</h2>
           {' '}
-          <FormControl type="text" placeholder="Archangel"/>
+          <FormControl type="text" placeholder="Archangel"
+          onChange={event => this.setState({name: event.target.value})}/>
         </FormGroup>
         {' '}
         <FormGroup>
-        <h2>Color</h2>
-        {' '}
-        <FormControl type="text" placeholder="White"/>
+          <h2>Cost</h2>
+          {' '}
+          <FormControl type="text" placeholder="White"
+          onChange={event => this.setState({cost: event.target.value})}/>
         </FormGroup>
         {' '}
-        <Button>Search</Button>
+        <Button onClick={()=> this.search()}>Search</Button>
       </Form>
     )
   }
