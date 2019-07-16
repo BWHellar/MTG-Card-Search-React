659e0fc32ed90dce40f2c2471c35b9d0e4663ce7
bh <bwhellar@gmail.com>
2019-07-16 15:41:21 -0700

    make the starring now work

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -3,19 +3,40 @@ import { connect } from 'react-redux';
 import { favoriteCard } from '../actions';
 
 class CardItem extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      favorited: false
+    }
+  }
+  
+  favorite(card) {
+    this.props.favoriteCard(card);
+    this.setState({favorited: true});
+  }
+  
+  
   render() {
     let { card } = this.props;
 
     return (
       <div className="card-item">
-      <div 
-        className="star"
-        onClick={() => this.props.favoriteCard(card)}>
-        &#9734;
-      </div>
+      {
+        this.state.favorited ?
+          <div className="star">&#9733;</div>
+        :
+        <div 
+          className="star"
+          onClick={() => this.favorite(card)}>
+          &#9734;
+        </div>
+      }
+      <div className="card-text">
         <a href={card.scryfall_uri}>
           <h4 className="card-text">{card.name}</h4>
         </a>
+      </div>
         <img 
         src={card.imageUrl} 
         alt={card.name} 
